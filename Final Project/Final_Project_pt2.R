522ca374dceb92f9ad842ecd0d7f1d7ce89fbe8b
StephenTYoung <95554685+StephenTYoung@users.noreply.github.com>
2022-06-03 22:49:26 -0400

    Week 8 & 9

---
+++ Final Project/Final_Project_pt2.R
@@ -0,0 +1,26 @@
+---
+title: "Final Project Step 1"
+output:
+  pdf_document: default
+date: '2022-06-03'
+---
+
+#How to import and clean my data
+
+#What does the final data set look like?
+
+#Questions for future steps.
+
+#What information is not self-evident?
+
+#What are different ways you could look at this data?
+
+#How do you plan to slice and dice the data?
+
+#How could you summarize your data to answer key questions?
+
+#What types of plots and tables will help you to illustrate the findings to your questions?
+
+#Do you plan on incorporating any machine learning techniques to answer your research questions? Explain.
+
+#Questions for future steps.
--- Week8-9/HousingSurvey_YoungStephen.R
+++ Week8-9/HousingSurvey_YoungStephen.R
@@ -5,10 +5,9 @@
 ## Set the working directory to the root of your DSC 520 directory
 setwd("C:/Users/young/Desktop/Classes/DSC520/GIT")
 
-## Load the `data/r4ds/heights.csv` to
+## Load the `housing data
 library(readxl)
 housing_df <- read_excel("data/week-7-housing.xlsx")
-
 
 #i. Explain any transformations or modifications you made to the dataset
 #I honestly do not remember if I did any transformations. I beleive there was a blank space that has been filled in. 
@@ -17,8 +16,7 @@ housing_df <- read_excel("data/week-7-housing.xlsx")
 #and one that will contain Sale Price and several additional predictors of your choice. Explain the basis for your additional predictor selections.
 names(housing_df)[names(housing_df)=='Sale Price'] <- 'sale_price'
 sale_vs_lot_lm <- lm(sale_price ~ sq_ft_lot, data = housing_df)
-
-sale_vs_zip_lm <- lm(sale_price ~ zip5, data = housing_df)
+sale_vs_zip_lm <- lm(sale_price ~ zip5+square_feet_total_living, data = housing_df)
 
 #iii. Execute a summary() function on two variables defined in the previous step to compare the model results. What are the R2 and Adjusted R2 statistics? 
 #Explain what these results tell you about the overall model. Did the inclusion of the additional predictors help explain any large variations found in Sale Price?
@@ -32,12 +30,21 @@ summary(sale_vs_zip_lm)
 
 
 #iv. Considering the parameters of the multiple regression model you have created. What are the standardized betas for each parameter and what do the values indicate?
-
+library(lm.beta)
+lm.beta(sale_vs_lot_lm)
+#The .1198122 indicates that as the sales price increases, so does the lot size of the property
+lm.beta(sale_vs_zip_lm)
+#With a .45297887 indicator, the square feet total living has a much greater impact than the .01681726 that the zip code has
 
 #v. Calculate the confidence intervals for the parameters in your model and explain what the results indicate.
+confint(sale_vs_lot_lm)
+#There is a small number for teh sq_ft_lot which indicates it is not a very good indicator 
 
+confint(sale_vs_zip_lm)
+#Both zip and square_feet_total_living have small numbers indicating that they would be very good indicators 
 
 #vi. Assess the improvement of the new model compared to your original model (simple regression model) by testing whether this change is significant by performing an analysis of variance.
+aov(housing_df$sq_ft_lot ~ square_feet_total_living, data = housing_df)
 
 
 #vii. Perform casewise diagnostics to identify outliers and/or influential cases, storing each function's output in a dataframe assigned to a unique variable name.
@@ -45,21 +52,25 @@ summary(sale_vs_zip_lm)
 
 #viii. Calculate the standardized residuals using the appropriate command, specifying those that are +-2, storing the results of large residuals in a variable you create.
 
+sales_lot_resid <- rstandard(sale_vs_lot_lm)
+summary(sales_lot_resid)
 
 #ix Use the appropriate function to show the sum of large residuals.
+sum(sales_lot_resid^2)
 
 
 #x Which specific variables have large residuals (only cases that evaluate as TRUE)?
 
 
 #xi Investigate further by calculating the leverage, cooks distance, and covariance rations. Comment on all cases that are problematics.
-obs_large_resid <- housing_df_cleaned[housing_df_cleaned$large_residuals,
-                                      c("standardized_residuals", "hat_values",
-                                        "cooks_distance", "covariance_ratio")]
 
-leverage_threshold <- (3 / nrow(housing_df)) * 3
-obs_large_resid$large_leverage <- obs_large_resid$hat_values > leverage_threshold
-sum(obs_large_resid$large_leverage)
+#sales_lot_resid <- housing_df[housing_df$large_residuals,
+#                                      c("standardized_residuals", "hat_values",
+#                                        "cooks_distance", "covariance_ratio")]
+
+#leverage_threshold <- (3 / nrow(housing_df)) * 3
+#sales_lot_resid$large_leverage <- sales_lot_resid$hat_values > leverage_threshold
+#sum(sales_lot_resid$large_leverage)
 
 #xii Perform the necessary calculations to assess the assumption of independence and state if the condition is met or not.
 
